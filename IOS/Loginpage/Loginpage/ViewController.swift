c089653570ae81b5945ed89a9dbdc9059d7f3f77
akash <akash2808@icloud.com>
2019-02-19 14:37:06 +0530

    added all ios files

---
+++ IOS/Loginpage/Loginpage/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Loginpage
+//
+//  Created by Akash Verma on 18/02/19.
+//  Copyright © 2019 Akash Verma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+    }
+
+
+}
+
