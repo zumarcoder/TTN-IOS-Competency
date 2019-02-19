bb4d2a39831c965e1c53c876e5063e57f422d708
akash <akash2808@icloud.com>
2019-02-19 16:02:46 +0530

    added 18thfeb asngmnt

---
+++ IOS/18thfeb/Loginpage1/Loginpage1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Loginpage1
+//
+//  Created by Akash Verma on 19/02/19.
+//  Copyright © 2019 Akash Verma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
